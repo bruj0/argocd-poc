713b85781e6959db3aa8a840ac6a09a809626fec
Rodrigo DL <ramakandra@gmail.com>
2025-01-27 13:41:32 +0100

    fixes

--- e2e/install/VARS.sh
+++ e2e/install/VARS.sh
@@ -1,3 +1,9 @@
 #INGRESS_IP=$(ip addr show $(ip route get 1.2.3.4 | grep -oP 'dev \K\S+') | grep -w 'inet' | awk '{print $2}' | cut -d/ -f1)    │
 INGRESS_IP="10.0.1.61"
 INGRESS_DOMAIN="${INGRESS_IP}.nip.io"
+
+## Gitea
+GITEA_HOST="gitea.${INGRESS_DOMAIN}"
+GITEA_USERNAME=gitea_admin
+GITEA_PASSWORD=
+GITEA_TOKEN=""
--- e2e/install/init-repos.sh
+++ e2e/install/init-repos.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 set -x
+. ./VARS.sh
 # initialize directories for repos appA, appB, appC, helm-charts
 repos=("appA" "appB" "appC" "helm-charts")
-gitea_url="http://gitea.10.0.2.15.nip.io/api/v1"
-gitea_token="ee56cac6c4583150767e620dd5c7ccdd3bd1bda9"
-gitea_user="gitea_admin"
+GITEA_API="http://${GITEA_HOST}/api/v1"
+GITEA_USERNAME="gitea_admin"
 
 for repo in "${repos[@]}"; do
   # Create repository in Gitea
-  curl -X POST "${gitea_url}/admin/users/${gitea_user}/repos" \
-    -H "Authorization: token ${gitea_token}" \
+  curl -X POST "${gitea_url}/admin/users/${GITEA_USERNAME}/repos" \
+    -H "Authorization: token ${GITEA_TOKEN}" \
     -H "Content-Type: application/json" \
     -d "{\"name\": \"${repo}\", \"private\": true}"
 sleep 1
   if [[ ! -d "repos/${repo}" ]]; then
     mkdir -p "repos/${repo}"
-    git clone "http://${gitea_user}:${gitea_token}@gitea.10.0.2.15.nip.io/${gitea_user}/${repo}.git" "repos/${repo}"
+    git clone "http://${GITEA_USERNAME}:${GITEA_TOKEN}@gitea.10.0.2.15.nip.io/${GITEA_USERNAME}/${repo}.git" "repos/${repo}"
   fi
 done
